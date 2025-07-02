0fce702709356021182259f4f3f47eb327b94a8f
aymenelmahboubi <elmahboubi.aymen2@gmail.com>
2025-07-02 15:43:27 +0100

    banner

--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div>
+      <div className="w-full h-[250px] bg-black flex items-center justify-center">
+        <h1 className="text-white text-4xl font-bold">FEATURES</h1>
+      </div>
+     
+    </div>
   )
 }
 
